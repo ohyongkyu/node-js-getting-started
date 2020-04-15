f176bed22a96609c759dae84efdc1c73742ab50a
ohyongkyu <evergreen.yk@gmail.com>
2020-04-15 23:50:28 +0900

    commit

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
+const app = express()
 const PORT = process.env.PORT || 5000
+const router = require('./routes/index')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('test'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use('/', router)
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+//app.engine('html', require('ejs').renderFile)
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
